57acc6d354cd6779d49a6caed9a3f6b65bb36be7
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2025-02-05 09:45:00 +0530

    refactor(core): add shared app string constants

---
+++ my_app/lib/core/constants/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const appName = 'Investment Tracker';
+  static const dashboardTitle = 'Investment Dashboard';
+  static const genericRetry = 'Retry';
+  static const emptyInvestments = 'No investments yet';
+}
