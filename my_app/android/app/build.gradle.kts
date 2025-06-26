b5e3a1d02852cace39222e441a8f0821ec971a67
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2025-06-26 16:00:00 +0000

    build: enable android and other platform builds

---
+++ my_app/android/app/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.my_app"
+    compileSdk = flutter.compileSdkVersion
+    // Pin NDK version to the installed side-by-side package to avoid IDE/AGP resolution issues
+    ndkVersion = "29.0.14206865"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.my_app"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // Disable code shrinking to avoid R8 missing-class issues
+            isMinifyEnabled = false
+             // Prevent resource shrinking since code shrinking is disabled
+            isShrinkResources = false
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
---
+++ my_app/android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Minimal ProGuard rules (shrinking is disabled in release; file kept for future use)
+# If you enable shrinking, avoid keeping Play Core dependent classes unless you add Play Core deps.
+# -dontwarn com.google.android.play.**
